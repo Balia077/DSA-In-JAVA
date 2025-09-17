fb51c31a7406aa492ea5ae5a28a66fb3fb592cb7
Balia077 <balaramd894@gmail.com>
2025-09-17 20:27:20 +0530

    Update selection sort input array for testing

--- Sorting/Selection Sort/SS.java
+++ Sorting/Selection Sort/SS.java
@@ -6,7 +6,7 @@ public class SS {
         System.out.println();
     }
     public static void main(String[] args) {
-        int arr[] = {7, 8, 3, 1, 2};
+        int arr[] = {0, 1, 2, 0, 1, 2};
 
         //Time Complexity = O(n^2)
 
